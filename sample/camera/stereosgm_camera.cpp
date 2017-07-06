3a2a976ec548daad0752afe724881b1eb8e6bc14
kishimoto-exvision <kishimoto@exvision.co.jp>
2017-07-06 16:15:59 +0900

    Update stereosgm_camera.cpp

--- sample/camera/stereosgm_camera.cpp
+++ sample/camera/stereosgm_camera.cpp
@@ -26,104 +26,92 @@ limitations under the License.
 #include "demo.h"
 #include "renderer.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
+    cv::VideoCapture leftCapture = cv::VideoCapture(0);
+    cv::VideoCapture rightCapture = cv::VideoCapture(1);
+    cv::Mat leftColor, rightColor;
+    cv::Mat left, right;
 
-	if (argc < 3) {
-		std::cerr << "usage: stereosgm left_img_fmt right_img_fmt [disp_size] [max_frame_num]" << std::endl;
-		std::exit(EXIT_FAILURE);
-	}
-	std::string left_filename_fmt, right_filename_fmt;
-	left_filename_fmt = argv[1];
-	right_filename_fmt = argv[2];
+    bool hr = leftCapture.read(leftColor);
+    hr = hr && rightCapture.read(rightColor);
+    if (hr == false) {
+        std::cerr << "It needs 2 cameras." << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+    if (left.size() != right.size() || left.type() != right.type()) {
+        std::cerr << "mismatch input image size" << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
 
-	// dangerous
-	char buf[1024];
-	sprintf(buf, left_filename_fmt.c_str(), 0);
-	cv::Mat left = cv::imread(buf, -1);
-	sprintf(buf, right_filename_fmt.c_str(), 0);
-	cv::Mat right = cv::imread(buf, -1);
+    cv::cvtColor(leftColor, left, CV_BGR2GRAY);
+    cv::cvtColor(rightColor, right, CV_BGR2GRAY);
+    int bits = 0;
+    switch (left.type()) {
+    case CV_8UC1: bits = 8; break;
+    case CV_16UC1: bits = 16; break;
+    default:
+        std::cerr << "invalid input image color format. Type: " << left.type() << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+
+    int disp_size = 64;
+    int width = left.cols;
+    int height = left.rows;
+
+    cudaGLSetGLDevice(0);
+
+    SGMDemo demo(width, height);
+
+    if (demo.init()) {
+        printf("fail to init SGM Demo\n");
+        std::exit(EXIT_FAILURE);
+    }
+
+    sgm::StereoSGM ssgm(width, height, disp_size, bits, 16, sgm::EXECUTE_INOUT_HOST2CUDA);
+
+    Renderer renderer(width, height);
+
+    uint16_t* d_output_buffer = NULL;
+
+    int frame_no = 0;
+    while (!demo.should_close())
+    {
+        glClearColor(0.0, 0.0, 0.0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
 
 
-	int disp_size = 64;
-	if (argc >= 4) {
-		disp_size = atoi(argv[3]);
-	}
+        bool hr = leftCapture.read(leftColor);
+        hr = hr && rightCapture.read(rightColor);
+        if (hr == false) {
+            std::cerr << "Failed capturing." << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
+        cv::cvtColor(leftColor, left, CV_BGR2GRAY);
+        cv::cvtColor(rightColor, right, CV_BGR2GRAY);
 
-	int max_frame = 100;
-	if(argc >= 5) {
-		max_frame = atoi(argv[4]);
-	}
+#if _DEBUG
+        cv::imshow("left", left);
+        cv::imshow("right", right);
+#endif
 
+        ssgm.execute(left.data, right.data, (void**)&d_output_buffer); // , sgm::DST_TYPE_CUDA_PTR, 16);
 
-	if (left.size() != right.size() || left.type() != right.type()) {
-		std::cerr << "mismatch input image size" << std::endl;
-		std::exit(EXIT_FAILURE);
-	}
+        switch (demo.get_flag()) {
+        case 0:
+        {
+            renderer.render_input((uint16_t*)left.data);
+        }
+        break;
+        case 1:
+            renderer.render_disparity(d_output_buffer, disp_size);
+            break;
+        case 2:
+            renderer.render_disparity_color(d_output_buffer, disp_size);
+            break;
+        }
 
-	int bits = 0;
-
-	switch (left.type()) {
-	case CV_8UC1: bits = 8; break;
-	case CV_16UC1: bits = 16; break;
-	default:
-		std::cerr << "invalid input image color format" << left.type() << std::endl;
-		std::exit(EXIT_FAILURE);
-	}
-
-	int width = left.cols;
-	int height = left.rows;
-
-	cudaGLSetGLDevice(0);
-
-	SGMDemo demo(width, height);
-
-	if (demo.init()) {
-		printf("fail to init SGM Demo\n");
-		std::exit(EXIT_FAILURE);
-	}
-
-	sgm::StereoSGM ssgm(width, height, disp_size, bits, 16, sgm::EXECUTE_INOUT_HOST2CUDA);
-
-	Renderer renderer(width, height);
-	
-	uint16_t* d_output_buffer = NULL;
-
-	int frame_no = 0;
-	while (!demo.should_close()) {
-
-		glClearColor(0.0, 0.0, 0.0, 1.0);
-		glClear(GL_COLOR_BUFFER_BIT);
-		
-		if (frame_no == max_frame) { frame_no = 0; }
-
-		sprintf(buf, left_filename_fmt.c_str(), frame_no);
-		cv::Mat left = cv::imread(buf, -1);
-		sprintf(buf, right_filename_fmt.c_str(), frame_no);
-		cv::Mat right = cv::imread(buf, -1);
-
-		if (left.size() == cv::Size(0, 0) || right.size() == cv::Size(0, 0)) {
-			max_frame = frame_no;
-			frame_no = 0;
-			continue;
-		}
-
-		ssgm.execute(left.data, right.data, (void**)&d_output_buffer); // , sgm::DST_TYPE_CUDA_PTR, 16);
-
-		switch (demo.get_flag()) {
-		case 0:
-			{
-				renderer.render_input((uint16_t*)left.data);
-			}
-			break;
-		case 1:
-			renderer.render_disparity(d_output_buffer, disp_size);
-			break;
-		case 2:
-			renderer.render_disparity_color(d_output_buffer, disp_size);
-			break;
-		}
-		
-		demo.swap_buffer();
-		frame_no++;
-	}
+        demo.swap_buffer();
+        frame_no++;
+    }
 }
